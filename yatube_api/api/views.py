cd059a005b82aa6b0541d6eaea42012382c9da92
Вячеслав Казаков <vakazakov-89@yandex.ru>
2022-07-24 15:11:00 +0300

    Delete custom permission from GroupViewSet, isort, delete empty file from migrations

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Group, Post
 from .permissions import IfAuthorCustomPermission
